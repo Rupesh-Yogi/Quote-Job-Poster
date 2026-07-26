6dd26dbcc404c2dcccd84ebd59add48f2a9e46e6
Rupesh Yogi <rupeshyogi712@gmail.com>
2026-07-26 07:20:08 +0100

    working on livesearch and style of result

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let secondsLeft = 30;
 
 // For Quote Card
@@ -20,11 +19,11 @@ const refreshButton = document.querySelector(".refresh-btn");
 
 refreshButton.addEventListener("click", () => {
   fetchQuote();
+  secondsLeft = 30;
   document.querySelector("#countDown").textContent = secondsLeft;
 });
 
 // For auto refresh & Countdown
-
 
 setInterval(() => {
   secondsLeft--;
@@ -35,3 +34,40 @@ setInterval(() => {
     secondsLeft = 30;
   }
 }, 1000);
+
+// For live Search Card
+
+const searchInput = document.querySelector(".search-input input");
+const countriesContainer = document.querySelector(".countries");
+
+let debounceTimer;
+
+searchInput.addEventListener("input", () => {
+  const inputValue = searchInput.value;
+
+  clearTimeout(debounceTimer);
+
+  debounceTimer = setTimeout(() => {
+    fetch(`https://countries.dev/name/${inputValue}`)
+      .then((res) => res.json())
+      .then((data) => {
+        countriesContainer.innerHTML = "";
+        data.forEach((element) => {
+          const containerForResult = document.createElement("div");
+          containerForResult.classList.add("containerForResult");
+
+          const img = document.createElement("img");
+          img.src = element.flags.png;
+          img.alt = "Flag";
+
+          const name = document.createElement("p");
+          name.textContent = element.name;
+
+          containerForResult.appendChild(img);
+          containerForResult.appendChild(name);
+
+          countriesContainer.appendChild(containerForResult);
+        });
+      });
+  }, 800);
+});
